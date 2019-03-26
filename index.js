9761e9de7b4ed827e47dcf42439a9ceddbfb6733
learningsteady0j0 <45418794+observer0J0@users.noreply.github.com>
2019-03-26 22:32:35 +0900

    홈페이지의 전체적인 틀 완성

---
+++ index.js
@@ -0,0 +1,31 @@
+var navList = document.getElementById("nav_list");
+var mainIframe = document.getElementById("main_iframe")
+
+navList.addEventListener('click', function (event) {
+    console.log(event.target.textContent);
+    target = event.target.textContent;
+    if (target === "Home") {
+        console.log(1);
+        mainIframe.setAttribute("src", "home.html");
+    }
+    else if (target === "Study Timeline") {
+        console.log(1);
+        mainIframe.setAttribute("src", "studyTimeline.html");
+    }
+    else if (target === "Daily Schedule") {
+        console.log(1);
+        mainIframe.setAttribute("src", "DailySchedule.html");
+    }
+    else if (target === "My Project") {
+        console.log(1);
+        mainIframe.setAttribute("src", "MyProject.html");
+    }
+    else if (target === "Memoirs") {
+        console.log(1);
+        mainIframe.setAttribute("src", "Memoirs.html");
+    }
+
+
+
+
+})
