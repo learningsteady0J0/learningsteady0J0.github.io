cde630cdeb7f3294eaacd05cdc809c65821a4dbf
learningsteady0j0 <45418794+observer0J0@users.noreply.github.com>
2019-03-26 22:47:56 +0900

    test

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var mainIframe = document.getElementById("main_iframe")
 navList.addEventListener('click', function (event) {
     console.log(event.target.textContent);
     target = event.target.textContent;
-    if (target === "Main Home") {
+    if (target === "Home") {
         console.log(1);
-        mainIframe.setAttribute("src", "Main Home.html");
+        mainIframe.setAttribute("src", "MainHome.html");
     }
     else if (target === "Study Timeline") {
         console.log(1);
